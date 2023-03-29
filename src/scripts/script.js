d7474c851491036dfe1c05056920ee2da0542e5e
Rafael <rafael_guilherme@hotmai.com>
2023-03-28 23:09:54 -0300

    filmes section

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,12 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-
-    fetch('https://imdb-api.com/API/Images/k_g0om8t4y/tt10366206')
-        .then(response => response.json())
-        .then(data => {
-            const imageUrl = data.items[data.items.length - 2].image
-            const imageHero = document.querySelector('#image-hero')
-            imageHero.src = imageUrl
-        })
-        .catch(error => console.log(error))
+//     wick4()
+//     wick1()
+//     wick2()
+//     wick3()
+    
+    
 })
+
+// //Banner do Hero
+// function wick4() {
+//     fetch('https://imdb-api.com/API/Images/k_g0om8t4y/tt10366206')
+//         .then(response => response.json())
+//         .then(data => {
+//             const imageUrl = data.items[data.items.length - 2].image
+//             const imageWick4 = data.items[data.items.length - 7].image
+//             const imageHero = document.querySelector('#image-hero')
+//             const imageBanner4 = document.querySelector('#john_wick4')
+//             imageHero.src = imageUrl
+//             imageBanner4.src = imageWick4
+//         })
+//         .catch(error => console.log(error))
+// }
+
+// //John Wick 1 
+// function wick1() {   
+//     fetch('https://imdb-api.com/pt-br/API/Title/k_g0om8t4y/tt2911666')
+//         .then(response => response.json())
+//         .then(data => {
+//             const imageWick1 = data.image
+//             const imageBanner1 = document.querySelector('#john_wick1')
+//             imageBanner1.src = imageWick1
+//         })
+//         .catch(error => console.log(error))
+// }
+
+// //John Wick 2 
+// function wick2() {   
+//     fetch('https://imdb-api.com/pt-br/API/Title/k_g0om8t4y/tt4425200')
+//         .then(response => response.json())
+//         .then(data => {
+//             const imageWick2 = data.image
+//             const imageBanner2 = document.querySelector('#john_wick2')
+//             imageBanner2.src = imageWick2
+//         })
+//         .catch(error => console.log(error))
+// }
+
+// //John Wick 3 
+// function wick3() {   
+//     fetch('https://imdb-api.com/pt-br/API/Title/k_g0om8t4y/tt6146586')
+//         .then(response => response.json())
+//         .then(data => {
+//             const imageWick3 = data.image
+//             const imageBanner3 = document.querySelector('#john_wick3')
+//             imageBanner3.src = imageWick3
+//         })
+//         .catch(error => console.log(error))
+//}
+
